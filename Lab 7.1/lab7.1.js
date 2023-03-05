cd4eeab0999bf43d33b15796f320e5cbc9570e0e
Jonathan Frances <jonathan.frances@live.com.au>
2023-03-05 14:52:14 +1100

    Update lab7.1.js

--- Lab 7.1/lab7.1.js
+++ Lab 7.1/lab7.1.js
@@ -8,6 +8,10 @@ var svg = d3.select("p")
     .attr("width", w)
     .attr("height", h)
     .style("outline", "solid thin skyblue");
+    
+line = d3.line()
+.x(function(d) { return xScale(d.date); })
+.y(function(d) { return yScale(d.number); });
 
 d3.csv("res/Unemployment_78-95.csv", function(d){
     return {
@@ -31,10 +35,6 @@ yScale = d3.scaleLinear()
     .domain([0, d3.max(dataset, function(d) { return d.number; })
     ])
     .range([h, 0]);
-
-line = d3.line()
-    .x(function(d) { return xScale(d.date); })
-    .y(function(d) { return yScale(d.number); });
 
 function lineChart(dataset){
     svg.append("path")
