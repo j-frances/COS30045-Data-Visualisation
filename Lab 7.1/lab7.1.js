5e76cd7b4a36020df525e139c26e2001be58a487
Jonathan Frances <jonathan.frances@live.com.au>
2023-03-05 14:35:49 +1100

    Upload files for lab task 7.1

---
+++ Lab 7.1/lab7.1.js
@@ -0,0 +1,46 @@
+var w = 600;
+var h = 300;
+
+var dataset
+
+d3.csv("res/Unemployment_78-95.csv", function(d){
+    return {
+        date: new Date(+d.year, +d.month-1),
+        number: +d.number
+    };
+}).then(function(data){
+    dataset = data;
+    lineChart(dataset);
+    console.log(dataset, ["date", "number"]);
+});
+
+xScale = d3.scaleTime()
+    .domain([
+        d3.min(dataset, function(d){ return d.date; }),
+        d3.max(dataset, function(d) { return d.date; })
+    ])
+    .range([0, w]);
+
+yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, function(d) { return d.number; })
+    ])
+    .range([h, 0]);
+
+line = d3.line()
+    .x(function(d) { return xScale(d.date); })
+    .y(function(d) { return yScale(d.number); });
+
+var svg = d3.select("p")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .style("outline", "solid thin skyblue");
+
+function lineChart(dataset){
+    svg.append("path")
+    .datum(dataset)
+    .attr("class", "line")
+    .attr("d", line);
+}           
+
+
