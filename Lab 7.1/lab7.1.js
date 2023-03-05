5a3b428372112a9d1e0227aa83ca22984daed1af
Jonathan Frances <jonathan.frances@live.com.au>
2023-03-05 15:34:59 +1100

    Update lab7.1.js

--- Lab 7.1/lab7.1.js
+++ Lab 7.1/lab7.1.js
@@ -1,29 +1,11 @@
 var w = 600;
 var h = 300;
 
-var dataset
-
 var svg = d3.select("p")
     .append("svg")
     .attr("width", w)
     .attr("height", h)
     .style("outline", "solid thin skyblue");
-
-xScale = d3.scaleTime()
-    .domain([
-        d3.min(dataset, function(d){ return d.date; }),
-        d3.max(dataset, function(d) { return d.date; })
-    ])
-    .range([0, w]);
-
-yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d) { return d.number; })
-    ])
-    .range([h, 0]);
-
-line = d3.line()
-.x(function(d) { return xScale(d.date); })
-.y(function(d) { return yScale(d.number); });
 
 d3.csv("res/Unemployment_78-95.csv", function(d){
     return {
@@ -31,16 +13,33 @@ d3.csv("res/Unemployment_78-95.csv", function(d){
         number: +d.number
     };
 }).then(function(data){
-    dataset = data;
+    var dataset = data;
     lineChart(dataset);
     console.table(dataset, ["date", "number"]);
 });
 
 function lineChart(dataset){
+    
+    xScale = d3.scaleTime()
+        .domain([
+            d3.min(dataset, function(d){ return d.date; }),
+            d3.max(dataset, function(d){ return d.date; })
+        ])
+        .range([0, w]);
+
+    yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, function(d) { return d.number; })
+        ])
+        .range([h, 0]);
+        
+    line = d3.line()
+        .x(function(d) { return xScale(d.date); })
+        .y(function(d) { return yScale(d.number); });
+
     svg.append("path")
-    .datum(dataset)
-    .attr("class", "line")
-    .attr("d", line);
+        .datum(dataset)
+        .attr("class", "line")
+        .attr("d", line);
 }           
 
 
