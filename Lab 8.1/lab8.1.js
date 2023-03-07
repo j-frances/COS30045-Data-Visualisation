0dea75b438ffebac8df42c120f92d2a3a6ac1be3
Jonathan Frances <jonathan.frances@live.com.au>
2023-03-07 18:38:30 +1100

    Update lab8.1.js

--- Lab 8.1/lab8.1.js
+++ Lab 8.1/lab8.1.js
@@ -2,15 +2,6 @@ var w = 1000;
 var h = 1000;
 var padding = 25;
 
-var yScale = d3.scaleLinear()
-    .domain([0, h])
-    .range([h, 0]);
-
-var xScale = d3.scaleBand()
-    .domain(d3.range(w))
-    .rangeRound([padding,w])
-    .paddingInner(0.05);
-
 var projection = d3.geoMercator()
     .center([145, -36.5])
     .translate([w / 2, h / 2])
@@ -30,11 +21,5 @@ d3.json("res/LGA_VIC.json").then(function(json){
         .data(json.features)
         .enter()
         .append()
-        .attr("x", function(d, i){
-            return xScale(i);
-        })
-        .attr("y", function(d, i){
-            return yScale(d[1]);
-        })
         .attr("d", path);
 })
