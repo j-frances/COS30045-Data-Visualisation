8f3f3a46dc2fa3dbc5f51f5b1b3df246b078e57d
Jonathan Frances <jonathan.frances@live.com.au>
2023-03-08 09:36:28 +1100

    Upload file for lab task 8.2

---
+++ Lab 8.2/lab8.2.js
@@ -0,0 +1,59 @@
+var w = 1000;
+var h = 1000;
+var padding = 25;
+
+var color = d3.scaleOrdinal()
+    .range(['#ffffcc','#a1dab4','#41b6c4','#2c7fb8','#253494']);
+
+var projection = d3.geoMercator()
+    .center([145,-36])
+    .translate([w / 2, h / 2])
+    .scale(5000);
+
+var path = d3.geoPath()
+    .projection(projection);
+
+var svg = d3.select("p")
+    .append("svg")
+    .attr("width", w + padding)
+    .attr("height", h + padding);
+
+d3.csv("res/VIC_LGA_unemployment.csv", function(d){
+    return {
+        LGA: +d.LGA,
+        unemployed: +d.unemployed
+    };
+}).then(function(data){
+
+    d3.json("res/LGA_VIC.json").then(function(json){
+
+        for(var i = 0; i < data.length; i++){
+            
+            var dataState = data[i].LGA;
+
+            var dataValue = parseFloat(data[i].unemployed);
+
+            for(var j = 0; j < json.features.length; j++){
+                
+                var jsonState = json.features[j].properties.LGA_name;
+
+                if(dataState == jsonState){
+
+                    json.features[j].properties.value = dataValue;
+
+                    break;
+                }
+            }
+        }
+
+        svg.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("stroke", "dimgray")
+            .attr("fill", function(d, i) {return color(i)})
+            .attr("d", path);
+    });
+
+    console.table(data, ["date", "number"]);
+});
