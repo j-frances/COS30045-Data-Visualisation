cac4da64505e450dbb72e9bb8b65b53bc5f9dbdc
Jonathan Frances <jonathan.frances@live.com.au>
2023-03-08 11:00:50 +1100

    Update lab8.2.js

--- Lab 8.2/lab8.2.js
+++ Lab 8.2/lab8.2.js
@@ -54,42 +54,42 @@ d3.csv("res/VIC_LGA_unemployment.csv", function(d){
             .attr("stroke", "dimgray")
             .attr("fill", function(d, i) {return color(i)})
             .attr("d", path);
-    });
 
-    d3.csv("res/VIC_city.csv", function(d){
-        return {
-            place: d.place,
-            lat: +d.lat,
-            long: +d.lon
-        }
-    }).then(function(data){
+        d3.csv("res/VIC_city.csv", function(d){
+            return {
+                place: d.place,
+                lat: +d.lat,
+                long: +d.lon
+            }
+        }).then(function(data){
+            
+            svg.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function(d, i){
+                return projection([d.long, d.lat])[0];
+            })
+            .attr("cy", function(d, i){
+                return projection([d.long, d.lat])[1];
+            })
+            .attr("r", 5);
         
-        svg.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", function(d, i){
-            return projection([d.long, d.lat])[0];
-        })
-        .attr("cy", function(d, i){
-            return projection([d.long, d.lat])[1];
-        })
-        .attr("r", 5);
-    
-        svg.selectAll("text")
-        .data(data)
-        .enter()
-        .append("text")
-        .attr("x", function(d, i){
-            return projection([d.long, d.lat])[0];
-        })
-        .attr("y", function(d, i){
-            return projection([d.long, d.lat])[1];
-        })
-        .style("fill", d3.color("yellow"))
-        .style("z-index", 1)
-        .text(function(d){
-            return d.place;
+            svg.selectAll("text")
+            .data(data)
+            .enter()
+            .append("text")
+            .attr("x", function(d, i){
+                return projection([d.long, d.lat])[0];
+            })
+            .attr("y", function(d, i){
+                return projection([d.long, d.lat])[1];
+            })
+            .style("fill", d3.color("yellow"))
+            .style("z-index", 1)
+            .text(function(d){
+                return d.place;
+            });
         });
     });
 });
