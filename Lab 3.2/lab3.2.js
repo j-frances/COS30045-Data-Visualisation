7844f421f8bde71cafabb21a1bbe5290cdb08e46
j-frances <jonathan.frances@live.com.au>
2023-03-30 12:56:48 +1100

    Update lab3.2.js

--- Lab 3.2/lab3.2.js
+++ Lab 3.2/lab3.2.js
@@ -2,6 +2,9 @@ var w = 1000;
 var h = 1000;
 var padding = 100;
 
+/* 
+    Initialise the data set of X and Y values.
+*/
 var dataset = [ 
     [5, 20], 
     [480, 90],  
@@ -15,6 +18,11 @@ var dataset = [
     [220, 88] 
     ];
 
+/* 
+    We first set up a scale for the X and Y axis to use.
+    We use D3 functions to get the min and max of the domain (data values).
+    We also establish the range within our SVG for the axes to be rendered.
+*/
 var xScale = d3.scaleLinear()
     .domain([d3.min(dataset, function(d){
         return d[0];
@@ -33,6 +41,11 @@ var yScale = d3.scaleLinear()
     })])
     .range([padding,h - padding])
 
+
+
+/* 
+    We then initialise the D3 axis object and assign the scale we just created.
+*/
 var xAxis = d3.axisBottom()
     .ticks(5)
     .scale(xScale);
@@ -41,13 +54,23 @@ var yAxis = d3.axisLeft()
     .ticks(5)
     .scale(yScale);
 
+
+/* 
+    Initialise the main SVG canvas.
+ */
 var svg = d3.select("body")
     .data(dataset)
     .append("svg")
     .attr("width", w)
     .attr("height", h)
     .style("outline", "solid thin skyblue");
-               
+           
+    
+/* 
+    We use the datapoints from our dataset to draw circles for our data.
+    The dataset is provided by d and we access the first (0) and second (1) elements
+    which represent the X and Y coordinates respectively.
+*/
 svg.selectAll("circle")
     .data(dataset)
     .enter()
@@ -67,6 +90,10 @@ svg.selectAll("circle")
         }
     });
 
+
+/* 
+    We also use the datapoints to map the text descriptions of the coordinates.
+*/
 svg.selectAll("text")
     .data(dataset)
     .enter()
@@ -83,6 +110,11 @@ svg.selectAll("text")
         return d[0] + ", " + d[1];
     });
 
+
+/* 
+    Finally we append the axes to our svg with appropriate transformation.
+    Without the padding to shift the axes they appear on the outer edges of the svg.
+*/
 svg.append("g")
 .attr("transform", "translate(0, "+(w - padding) +")")
 .call(xAxis);
